98b8e05ebc1cec16a46934a318e1d26a74a324c3
Vaios-S <vaios.sav.dev@gmail.com>
2026-05-10 16:53:20 +0300

    add global theme and sidebar initialization

---
+++ global.js
@@ -0,0 +1,30 @@
+//Fetch Sidebar
+
+function fetchSideBar() {
+  fetch("sidebar.html")
+    .then((response) => response.text())
+    .then((sidebarHtml) => {
+      document.getElementById("sidebar-container").innerHTML = sidebarHtml;
+
+      const currentPage = window.location.pathname;
+      const links = document.querySelectorAll(".nav-item");
+
+      links.forEach((link) => {
+        link.classList.remove("nav-item--active");
+
+        if (currentPage.includes(link.getAttribute("href"))) {
+          link.classList.add("nav-item--active");
+        }
+      });
+    });
+}
+fetchSideBar();
+
+// Toggle Theme
+
+let theme = localStorage.getItem("theme");
+if (theme === "dark") {
+  document.body.dataset.theme = "dark";
+} else {
+  document.body.dataset.theme = "light";
+}
